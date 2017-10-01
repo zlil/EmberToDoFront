a1ab2a2431410fc05cca0686671a253d50feac5c
zlil <zlil.korman@gmail.com>
2017-10-02 00:07:36 +0300

    * add service * make api call * create controller

---
+++ app/controllers/all.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  repo: Ember.inject.service(),
+  newToDo: '',
+  actions: {
+    createTodo(e) {
+      debugger;
+          let description = this.get('newToDo');
+          var self = this;
+        this.get('repo').add({ description: description, completed: false }, self);
+        e.target.value = '';
+
+    },
+  }
+});
